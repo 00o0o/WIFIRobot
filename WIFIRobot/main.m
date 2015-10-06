afb797dcd6b02b16966185d4abc05c94c85089cf
mac <mac@macdemac.local>
2015-10-06 20:49:39 +0800

    Initial Commit

---
+++ WIFIRobot/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WIFIRobot
+//
+//  Created by mac on 10/6/15.
+//  Copyright © 2015 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
